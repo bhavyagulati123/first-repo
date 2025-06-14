823322e13c0961545002cc3cd345951dc454a324
bhavyagulati123 <119064078+bhavyagulati123@users.noreply.github.com>
2025-06-14 13:03:01 +0530

    counter

---
+++ my-next-app/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">About Page</h1>
+      <p>This is the About page content.</p>
+    </div>
+  );
+}
